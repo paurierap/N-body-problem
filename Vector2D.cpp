c24c5d39c5b8f9d4e6f35005139a024d6a5c2665
Pau Riera <pau.riera.p@gmail.com>
2025-05-09 17:20:45 +0100

    Include exceptions and missing arithmetic in Vector2D

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -2,7 +2,7 @@
 
 #include "Vector2D.hpp"
 #include <cmath>
-
+#include <stdexcept>
 
 // Constructors:
 Vector2D::Vector2D() : x_(0), y_(0) {}
@@ -19,29 +19,60 @@ Vector2D& Vector2D::operator=(const Vector2D& vec){
 
 
 // Arithmetic operators
+Vector2D& Vector2D::operator+=(const Vector2D& other) {
+    x_ += other.x_;
+    y_ += other.y_;
+    return *this;
+}
+
+Vector2D& Vector2D::operator-=(const Vector2D& other) {
+    x_ -= other.x_;
+    y_ -= other.y_;
+    return *this;
+}
+
+Vector2D& Vector2D::operator*=(double scalar) {
+    x_ *= scalar;
+    y_ *= scalar;
+    return *this;
+}
+
+Vector2D& Vector2D::operator/=(double scalar) {
+    if (scalar == 0) throw std::runtime_error("Division by zero");
+    x_ /= scalar;
+    y_ /= scalar;
+    return *this;
+}
+
 Vector2D operator+(const Vector2D& u, const Vector2D& v)
 {
-    Vector2D w(u.x_ + v.x_, u.y_ + v.y_);
-    return w;
+    return Vector2D(u) += v;
 }
 
 Vector2D operator-(const Vector2D& u, const Vector2D& v)
 {
-    Vector2D w(u.x_ - v.x_, u.y_ - v.y_);
-    return w;
+    return Vector2D(u) -= v;
 }
 
 Vector2D operator*(const Vector2D& u, double scalar)
 {
-    Vector2D w(scalar * u.x_, scalar * u.y_);
-    return w;
+    return Vector2D(u) *= scalar;
 }
 
 Vector2D operator*(double scalar, const Vector2D& u)
 {
-    return u * scalar;
+    return Vector2D(u) *= scalar;
 }
 
+Vector2D operator/(const Vector2D& u, double scalar)
+{
+    return Vector2D(u) /= scalar;
+}
+
+Vector2D operator/(double scalar, const Vector2D& u)
+{
+    return Vector2D(u) /= scalar;
+}
 
 // Euclidean norm:
 double norm(const Vector2D& vec)
@@ -57,10 +88,15 @@ double& Vector2D::operator[](int index) {
     throw std::out_of_range("Index out of range: Vector2D only supports indices 0 and 1");
 }
 
+double Vector2D::operator[](int index) const {
+    if (index == 0) return x_;
+    if (index == 1) return y_;
+    throw std::out_of_range("Index out of range: Vector2D only supports indices 0 and 1");
+}
 
 // Print vector formatted:
 std::ostream& operator<<(std::ostream& output, const Vector2D& vec)
 {
-    output << "(" << vec.x_ << ", " << vec.y_ << ")" << std::endl;
+    output << "(" << vec.x_ << ", " << vec.y_ << ")" << "\n";
     return output;
 }
--- Vector2D.hpp
+++ Vector2D.hpp
@@ -15,15 +15,22 @@ public:
 
     // Assignment
     Vector2D& operator=(const Vector2D&);
+    Vector2D& operator+=(const Vector2D&);
+    Vector2D& operator-=(const Vector2D&);
+    Vector2D& operator*=(double);
+    Vector2D& operator/=(double);
 
     // Access operator
     double& operator[](int);
+    double operator[](int) const;
     
     // Overload arithmetic operators
     friend Vector2D operator+(const Vector2D&, const Vector2D&);
     friend Vector2D operator-(const Vector2D&, const Vector2D&);
     friend Vector2D operator*(double, const Vector2D&);
     friend Vector2D operator*(const Vector2D&, double);
+    friend Vector2D operator/(double, const Vector2D&);
+    friend Vector2D operator/(const Vector2D&, double);
 
     // Get norm of a vector
     friend double norm(const Vector2D&);
